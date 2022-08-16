e0f2a6ebeb0f313da87804f05bb82ac3359096ce
nehru35 <sonehru@gmail.com>
2022-08-16 10:39:05 +0100

    Extra

--- src/components/PessoaComponent.vue
+++ src/components/PessoaComponent.vue
@@ -2,22 +2,25 @@
     <div>
         <h1>Está é a descrição da pessoa: {{ nome }}</h1>
         <InfoComponent />
+
+        <p v-if="mostra">Trabalhando...</p>
+        <p v-else>Não trabalhando...</p>
+        <p v-show="mostra">Trabalhando...</p>
     </div>
 </template>
 
 <script>
     import infoComponent from './infoComponent.vue';
-import InfoComponent from './infoComponent.vue';
 
     export default {
         name: "PessoaComponet",
         components: {
-    infoComponent,
-    InfoComponent
-},
+        infoComponent
+    },
         data() {
             return {
-                nome: 'Matheus'
+                nome: 'Matheus',
+                mostra: true
             }
         }
     }

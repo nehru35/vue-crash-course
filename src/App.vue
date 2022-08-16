b9d58b01432d3db3baceb0f828782177dd6bb66b
nehru35 <sonehru@gmail.com>
2022-08-16 01:09:17 +0100

    Aula 06 - Dados em componentes
    Aprendendo criar variaveis, methodos dentro do data()

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <PrimeiroComponent />
+    <p>Morada: {{ morada }}</p>
+  </div>
+</template>
+
+<script>
+  import PrimeiroComponent from './components/PrimeiroComponent.vue';
+
+
+  export default {
+    name: 'App',
+    components: {
+      PrimeiroComponent
+    },
+    data () {
+      return {
+        morada: 'Gamek'
+      }
+    }
+  }
+</script>
---
+++ src/components/PrimeiroComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Nome: {{ nome }}</p>
+        <p>Profissão: {{ profissao }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponent',
+        data () {
+            return {
+                nome: 'Nehrú',
+                profissao: 'Programador'
+            }
+        }
+    }
+</script>
